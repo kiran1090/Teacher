27ebb3995109b72033f746a9f5f5e26010b1b31e
kiran_a <iamkiranashok@gmail.com>
2022-12-15 22:21:48 +0530

    Teacher class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public  static void main(String[] arg)
+    {
+Teacher teacherOne= new Teacher("raju","bsc");
+Teacher teacherTwo= new Teacher("ram","ba");
+
+    System.out.println(teacherTwo);
+    }
+}
---
+++ src/Teacher.java
@@ -0,0 +1,10 @@
+public class Teacher {
+    private String name;
+    private String department;
+    Teacher (String name,String department)
+    {
+        this.name=name;
+        this.department=department;
+
+    }
+}
